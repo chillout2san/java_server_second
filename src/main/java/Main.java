f07e50e554b9f6000f18746f62ac956b32d462ca
chillout2san <chillout2san@gmail.com>
2023-03-25 15:11:35 +0900

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import presentation.Router;
+
+public class Main {
+  public static void main(String[] args) {
+    Router.execute();
+  }
+}
---
+++ src/main/java/presentation/Router.java
@@ -0,0 +1,16 @@
+package presentation;
+
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Server;
+
+public class Router {
+  public static void execute() {
+    var serverBuilder = Server.builder();
+    serverBuilder.http(8080);
+    serverBuilder.service("/", (ctx, req) -> HttpResponse.of("Hello world"));
+
+    var server = serverBuilder.build();
+    var future = server.start();
+    future.join();
+  }
+}
