feb31388e6507b61ce6c57f6a4f65191154914e9
chillout2san <chillout2san@gmail.com>
2023-03-26 18:55:55 +0900

    TodoのGET実装

--- src/main/java/presentation/todo/TodoController.java
+++ src/main/java/presentation/todo/TodoController.java
@@ -1,12 +1,16 @@
 package presentation.todo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linecorp.armeria.common.HttpMethod;
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.common.HttpStatus;
 import com.linecorp.armeria.common.MediaType;
 import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.server.annotation.ProducesJson;
+import domain.todo.Todo;
 import infrastructure.todo.TodoRepositoryImpl;
+import java.util.List;
 import org.apache.ibatis.session.SqlSessionFactory;
 import presentation.NotFoundPresentationException;
 import usecase.todo.GetUseCase;
@@ -21,17 +25,39 @@ public class TodoController {
       throw new NotFoundPresentationException("method not found");
     }));
   }
-
-  @ProducesJson
+  
   private static HttpResponse get(SqlSessionFactory sqlSessionFactory) {
     try {
       var todoRepository = new TodoRepositoryImpl(sqlSessionFactory);
       var getUseCase = new GetUseCase(todoRepository);
       var todos = getUseCase.execute();
-      return HttpResponse.of(HttpStatus.OK, MediaType.JSON_UTF_8, "{\"result\": {\"todos\":%v}",
-          todos);
+      var response = new GetResponse(todos);
+      var objectMapper = new ObjectMapper();
+      var json = objectMapper.writeValueAsString(response);
+      return HttpResponse.of(HttpStatus.OK, MediaType.JSON_UTF_8, json);
     } catch (Exception e) {
       return HttpResponse.of(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
+
+class GetResponse {
+
+  @JsonProperty("result")
+  private Todos result;
+
+  public GetResponse(List<Todo> value) {
+    var todos = new Todos(value);
+    this.result = todos;
+  }
+
+  class Todos {
+
+    @JsonProperty("todos")
+    private List<Todo> value;
+
+    private Todos(List<Todo> todos) {
+      this.value = todos;
     }
   }
 }
